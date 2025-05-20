fcd295a992e25f827d987bdfd408a78e614136ba
fedebacci <federicobacci1999@gmail.com>
2025-05-20 16:58:14 +0200

    creazione struttura base

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,67 @@
 export default function Header () {
-    return <header>Header</header>
+    return <header id="page-header">
+        <div className="container">
+            <div className="row">
+                <div className="col-100">
+                    <figure>
+                        <img src="/img/dc-logo.png" alt="DC Logo" className="logo" />
+                    </figure>
+                    <nav id="main-menu">
+                        <ul>
+                            <li>
+                                <a href="#">
+                                    CHARACTERS
+                                </a>
+                            </li>
+                            <li className="active">
+                                <a href="#">
+                                    COMICS
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    MOVIES
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    TV
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    GAMES
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    COLLECTIBLES
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    VIDEOS
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    FANS
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    NEWS
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    SHOP
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,51 @@
 export default function Main () {
-    return <main>main</main>
+    return <main id="page-main">
+        <section id="main-content">
+            <div className="container">
+                <div className="row">
+                    <div className="col-100">
+                        <h2>
+                            &rarr; Content goes here &larr;
+                        </h2>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="merchandise">
+            <div className="container">
+                <div className="row">
+                    <div className="col-1-5">
+                        <div className="merchandise-box">
+                            <img src="/img/buy-comics-digital-comics.png" alt="buy-comics-digital-comics" />
+                            DIGITAL COMICS
+                        </div>
+                    </div>
+                    <div className="col-1-5">
+                        <div className="merchandise-box">
+                            <img src="/img/buy-comics-merchandise.png" alt="buy-comics-merchandise" />
+                            DC MERCHANDISE
+                        </div>
+                    </div>
+                    <div className="col-1-5">
+                        <div className="merchandise-box">
+                            <img src="/img/buy-comics-subscriptions.png" alt="buy-comics-subscriptions" />
+                            SUBSCRIPTION
+                        </div>
+                    </div>
+                    <div className="col-1-5">
+                        <div className="merchandise-box">
+                            <img src="/img/buy-comics-shop-locator.png" alt="buy-comics-shop-locator" />
+                            COIC SHOP LOCATOR
+                        </div>
+                    </div>
+                    <div className="col-1-5">
+                        <div className="merchandise-box">
+                            <img src="/img/buy-dc-power-visa.svg" alt="buy-dc-power-visa" />
+                            DC POWER VISA
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
 }
