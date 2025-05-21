c328fb390de2cfe805c5b2e24d541a1d4f676431
fedebacci <federicobacci1999@gmail.com>
2025-05-21 14:04:08 +0200

    divisione main in componenti

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,51 +1,9 @@
+import MainContent from "./MainContent";
+import Merchandise from "./Merchandise";
+
 export default function Main () {
     return <main id="page-main">
-        <section id="main-content">
-            <div className="container">
-                <div className="row">
-                    <div className="col-100">
-                        <h2>
-                            &rarr; Content goes here &larr;
-                        </h2>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <section id="merchandise">
-            <div className="container">
-                <div className="row">
-                    <div className="col-1-5">
-                        <div className="merchandise-box">
-                            <img src="/img/buy-comics-digital-comics.png" alt="buy-comics-digital-comics" />
-                            <span>DIGITAL COMICS</span>
-                        </div>
-                    </div>
-                    <div className="col-1-5">
-                        <div className="merchandise-box">
-                            <img src="/img/buy-comics-merchandise.png" alt="buy-comics-merchandise" />
-                            <span>DC MERCHANDISE</span>
-                        </div>
-                    </div>
-                    <div className="col-1-5">
-                        <div className="merchandise-box">
-                            <img src="/img/buy-comics-subscriptions.png" alt="buy-comics-subscriptions" />
-                            <span>SUBSCRIPTION</span>
-                        </div>
-                    </div>
-                    <div className="col-1-5">
-                        <div className="merchandise-box">
-                            <img src="/img/buy-comics-shop-locator.png" alt="buy-comics-shop-locator" />
-                            <span>COMIC SHOP LOCATOR</span>
-                        </div>
-                    </div>
-                    <div className="col-1-5">
-                        <div className="merchandise-box">
-                            <img src="/img/buy-dc-power-visa.svg" alt="buy-dc-power-visa" />
-                            <span>DC POWER VISA</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
+        <MainContent />
+        <Merchandise />
     </main>
 }
---
+++ src/components/MainContent.jsx
@@ -0,0 +1,13 @@
+export default function MainContent () {
+return  <section id="main-content">
+            <div className="container">
+                <div className="row">
+                    <div className="col-100">
+                        <h2>
+                            &rarr; Content goes here &larr;
+                        </h2>
+                    </div>
+                </div>
+            </div>
+        </section>
+}
